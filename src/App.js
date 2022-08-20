cfcc69bf1a823e54b2df9b667357b6d08230ffa6
xodnr11 <971021q@naver.com>
2022-08-21 05:55:30 +0900

    react 개발환경 구축 component, props, state, event 실습

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import Content from './components/Content';
+import Subject from './components/Subject';
+import TOC from './components/TOC';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      mode:'welcome',
+      subject:{title:"Web", sub:'World Wide Web!'},
+      contents:[
+        {id:1, title:'HTML', decs:'HTML is for information ...'},
+        {id:2, title:'CSS', decs:'CSS is for design ...'},
+        {id:3, title:'JavaScript', decs:'JavaScript is for interactive ...'}
+      ]
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <Subject 
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}>
+
+        </Subject>
+        <TOC data={this.state.contents}></TOC>
+        <Content title="html"></Content>
+      </div>
+    );
+  }
 }
 
 export default App;
